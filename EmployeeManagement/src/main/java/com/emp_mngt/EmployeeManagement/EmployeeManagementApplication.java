3ffc0fbe85b89daede9f1aaeb545a49c057d7515
Tanmay Kanase <tanmaykanase06@gmial.com>
2026-05-26 12:36:35 +0530

    Initial Commit

---
+++ EmployeeManagement/src/main/java/com/emp_mngt/EmployeeManagement/EmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.emp_mngt.EmployeeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+}
