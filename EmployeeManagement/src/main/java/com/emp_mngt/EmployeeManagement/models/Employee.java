5e4d591dd438444411136383d59da030187cf4f8
Tanmay Kanase <tanmaykanase06@gmial.com>
2026-05-26 12:37:04 +0530

    Employee model for table

---
+++ EmployeeManagement/src/main/java/com/emp_mngt/EmployeeManagement/models/Employee.java
@@ -0,0 +1,29 @@
+package com.emp_mngt.EmployeeManagement.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private Double salary;
+
+}
