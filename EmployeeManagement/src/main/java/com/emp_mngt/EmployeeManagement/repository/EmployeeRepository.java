21efbd43d4c387eeeac2ea4459ced62d1e4cf34b
Tanmay Kanase <tanmaykanase06@gmial.com>
2026-05-26 12:37:23 +0530

    Repository

---
+++ EmployeeManagement/src/main/java/com/emp_mngt/EmployeeManagement/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.emp_mngt.EmployeeManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.emp_mngt.EmployeeManagement.models.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
